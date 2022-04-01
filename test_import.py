d18c97c6ea92ce72c011eb972c5043ef2832be4a
alexjette <99074074+alexjette@users.noreply.github.com>
2022-04-01 13:58:29 +0200

    update

--- test_import.py
+++ test_import.py
@@ -2,7 +2,8 @@ from tables import allowedPackaging
 from tables import utilization
 from tables import packaging
 from tables import articles
-from facility import usedPackagingMatrix
+from tables import partDemand
+#from facility import usedPackagingMatrix
 import xlsxwriter as xl
 import pandas as pd
 import numpy as np
@@ -20,15 +21,17 @@ for p in packaging:
     row.append(f'Box {str(packaging[row_index])}')
     row_index += 1
 
-usedPackagingMatrix_1 = pd.DataFrame(usedPackagingMatrix.x, columns=col)
-usedPackagingMatrix_1.index = row
+demand_table = pd.DataFrame(partDemand, columns=['Demand'])
+demand_table.index = col
+#usedPackagingMatrix_1 = pd.DataFrame(usedPackagingMatrix.x, columns=col)
+#usedPackagingMatrix_1.index = row
 checkPackaging = pd.DataFrame(allowedPackaging, columns=col)
 checkPackaging.index = row
 utilizationTable = pd.DataFrame(utilization, columns=col)
 utilizationTable.index = row
-print('Used Packaging: \n', usedPackagingMatrix_1)
-print('Check Packaging: \n', checkPackaging)
-print('Utilization: \n', utilizationTable)
+#print('Used Packaging: \n', usedPackagingMatrix_1)
+#print('Check Packaging: \n', checkPackaging)
+#print('Utilization: \n', utilizationTable)
 
 def create_woorkbook():
 
@@ -36,7 +39,8 @@ def create_woorkbook():
 
     checkPackaging.to_excel(workbook, sheet_name='Check Packaging', index=row)
     utilizationTable.to_excel(workbook, sheet_name='Utilization Table', index=row)
-    usedPackagingMatrix_1.to_excel(workbook, sheet_name='Used Packaging Table', index=row)
+    #usedPackagingMatrix_1.to_excel(workbook, sheet_name='Used Packaging Table', index=row)
+    demand_table.to_excel(workbook, sheet_name='Demand Table', index=col)
 
     workbook.close()
 
